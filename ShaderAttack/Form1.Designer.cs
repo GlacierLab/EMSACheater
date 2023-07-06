3ea71892d7c33b1eda0d57d87dd40911432f58ab
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-07-06 11:23:41 +1000

    支持修改生命的第一个版本

---
+++ ShaderAttack/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ShaderAttack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            OpenProcess = new Button();
+            ServiceSwitch = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // OpenProcess
+            // 
+            OpenProcess.Location = new Point(12, 12);
+            OpenProcess.Name = "OpenProcess";
+            OpenProcess.Size = new Size(776, 34);
+            OpenProcess.TabIndex = 0;
+            OpenProcess.Text = "附加进程";
+            OpenProcess.UseVisualStyleBackColor = true;
+            OpenProcess.Click += OpenProcess_Click;
+            // 
+            // ServiceSwitch
+            // 
+            ServiceSwitch.Enabled = false;
+            ServiceSwitch.Location = new Point(12, 52);
+            ServiceSwitch.Name = "ServiceSwitch";
+            ServiceSwitch.Size = new Size(776, 34);
+            ServiceSwitch.TabIndex = 2;
+            ServiceSwitch.Text = "启动服务";
+            ServiceSwitch.UseVisualStyleBackColor = true;
+            ServiceSwitch.Click += ServiceSwitch_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(297, 25);
+            label1.TabIndex = 3;
+            label1.Text = "输入数字后按下回车即可修改生命";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(ServiceSwitch);
+            Controls.Add(OpenProcess);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button OpenProcess;
+        private Button ServiceSwitch;
+        private Label label1;
+    }
+}
---
+++ ShaderAttack/Form1.cs
@@ -0,0 +1,117 @@
+using Binarysharp.MemoryManagement;
+using Gma.System.MouseKeyHook;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ShaderAttack
+{
+    public partial class Form1 : Form
+    {
+        private Process? GameProcess;
+        private MemorySharp? MSharp;
+        private IKeyboardMouseEvents? m_GlobalHook;
+        private string Hearts = "";
+
+        [DllImport("kernel32.dll")]
+        private static extern int Beep(int dwFreq, int dwDuration);
+
+        public enum Music
+        {
+            Do = 523,
+            Re = 587,
+            Mi = 659,
+            Fa = 698,
+            So = 784,
+            La = 880,
+            Ti = 988,
+            Do2 = 1046
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenProcess_Click(object sender, EventArgs e)
+        {
+            GameProcess = null;
+            switch (Process.GetProcessesByName("Em_android").Length)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        GameProcess = Process.GetProcessesByName("Em_android")[0];
+                        break;
+                    }
+                default:
+                    {
+                        foreach (Process ProcessToChoose in Process.GetProcessesByName("Em_android"))
+                        {
+                            DialogResult dialogResult = MessageBox.Show("你要选择的进程是不是:" + ProcessToChoose.Id.ToString(), "选择进程", MessageBoxButtons.YesNo);
+                            if (dialogResult == DialogResult.Yes)
+                            {
+                                GameProcess = ProcessToChoose;
+                                break;
+                            }
+                            else if (dialogResult == DialogResult.No)
+                            {
+                                continue;
+                            }
+                        }
+                        break;
+                    }
+            }
+            if (GameProcess != null)
+            {
+                OpenProcess.Text = "当前选择进程:" + GameProcess.Id.ToString();
+                MSharp = new MemorySharp(GameProcess);
+                ServiceSwitch.Enabled = true;
+            }
+            else {
+                MessageBox.Show("未找到游戏进程！");
+                    }
+
+        }
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", (int)e.KeyChar);
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+            {
+                Hearts += e.KeyChar.ToString();
+                Beep((int)Music.Do, 300);
+            }
+            if (e.KeyChar == 13&&Hearts!="")
+            {
+                MSharp[0x02156854, false].Write(Int32.Parse(Hearts));
+                Hearts = "";
+                Beep((int)Music.Do2, 800);
+            }
+        }
+
+        private void AddHeart_Click(object sender, EventArgs e)
+        {
+            MSharp[0x02156854, false].Write(8);
+        }
+
+        private void ServiceSwitch_Click(object sender, EventArgs e)
+        {
+            if (m_GlobalHook == null)
+            {
+                m_GlobalHook = Hook.GlobalEvents();
+                m_GlobalHook.KeyPress += GlobalHookKeyPress;
+                ServiceSwitch.Text = "停止服务";
+            }
+            else
+            {
+                m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+                m_GlobalHook.Dispose();
+                m_GlobalHook = null;
+                ServiceSwitch.Text = "启动服务";
+            }
+
+        }
+    }
+}
