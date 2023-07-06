e6d3fda0800f831ff943db9a4d0f65d5526ba37d
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-07-06 15:22:32 +1000

    添加异常捕捉

--- ShaderAttack/Program.cs
+++ ShaderAttack/Program.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ShaderAttack
 {
     internal static class Program
@@ -11,7 +13,39 @@ namespace ShaderAttack
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+                             Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+                             //处理非UI线程异常
+                 AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
             Application.Run(new Form1());
         }
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+             string str = GetExceptionMsg(e.Exception, e.ToString());
+             MessageBox.Show(str, "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+         }
+ 
+         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+         {
+             string str = GetExceptionMsg(e.ExceptionObject as Exception, e.ToString());
+             MessageBox.Show(str, "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+         }
+        static string GetExceptionMsg(Exception ex, string backStr)
+         {
+                 StringBuilder sb = new StringBuilder();
+                 sb.AppendLine("****************************异常文本****************************");
+                 sb.AppendLine("【出现时间】：" + DateTime.Now.ToString());
+                 if (ex != null)
+                    {
+                         sb.AppendLine("【异常类型】：" + ex.GetType().Name);
+                        sb.AppendLine("【异常信息】：" + ex.Message);
+                         sb.AppendLine("【堆栈调用】：" + ex.StackTrace);
+                    }
+                 else
+                    {
+                        sb.AppendLine("【未处理异常】：" + backStr);
+                    }
+               sb.AppendLine("***************************************************************");
+                 return sb.ToString();
+            }
     }
 }
